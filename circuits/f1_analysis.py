4ec9bdc94336086c3c3518089cf6f5516e3d2d39
Andrew Quaisley <andrew.quaisley@gmail.com>
2024-10-25 11:55:34 -0700

    test

--- circuits/f1_analysis.py
+++ circuits/f1_analysis.py
@@ -154,6 +154,7 @@ def calculate_all_sae_coverage(
 
             print(f"Processing {autoencoder_path}")
 
+            print(df["autoencoder_path"].values) 
             assert (
                 autoencoder_path in df["autoencoder_path"].values
             ), f"{autoencoder_path} not in csv file"
